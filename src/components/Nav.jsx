1a4f360fc10187a6dca688c60260109b78b229c9
kristian <Kristian.larsen38@gmail.com>
2025-03-03 20:13:57 +0100

    push igjen

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,12 @@ import { showInfo } from "../resources/ressurser.js";
 
 export default function Nav() {
 return (
-    <nav>
-        <button className="button" onClick={() => showInfo('HTML')}></button>
-        <button className="button" onClick={() => showInfo('CSS')}></button>
-        <button className="button" onClick={() => showInfo('JavaScript')}>JavaScript</button>
-        <button className="button" onClick={() => showInfo('React')}>React</button>
-        <button className="button" onClick={() => showInfo('Sanity and headless CMS')}>Sanity and headless CMS</button>
-    </nav>
+   <nav>
+        <ul>
+            <li>
+                <a href=¤"></a>
+            </li>
+        </ul>
+   </nav>
 )
 }
