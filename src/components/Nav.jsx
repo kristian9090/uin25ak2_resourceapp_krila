aaa6282a99672b32751a1ab7f2c90002d2de4eb0
kristian <Kristian.larsen38@gmail.com>
2025-03-10 20:42:36 +0100

    mangler bare i venstrestille

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,24 +15,32 @@ export default function Nav({ category }) {
     }
 
     return (
-        
         <nav>
-            
-            <button className={`button ${activeCategory === 'html' ? 'active' : ''}`} onClick={() => handleButtonClick('html')}>
-                <Link to="/html">HTML</Link>
-            </button>
-            <button className={`button ${activeCategory === 'css' ? 'active' : ''}`} onClick={() => handleButtonClick('css')}>
-                <Link to="/css">CSS</Link>
-            </button>
-            <button className={`button ${activeCategory === 'javascript' ? 'active' : ''}`} onClick={() => handleButtonClick('javascript')}>
-                <Link to="/javascript">JavaScript</Link>
-            </button>
-            <button className={`button ${activeCategory === 'react' ? 'active' : ''}`} onClick={() => handleButtonClick('react')}>
-                <Link to="/react">React</Link>
-            </button>
-            <button className={`button ${activeCategory === 'headless-cms' ? 'active' : ''}`} onClick={() => handleButtonClick('headless-cms')}>
-                <Link to="/headless-cms">Sanity and headless CMS</Link>
-            </button>
+            <Link to="/html">
+                <button className={`button ${activeCategory === 'html' ? 'active' : ''}`} onClick={() => handleButtonClick('html')}>
+                    HTML
+                </button>
+            </Link>
+            <Link to="/css">
+                <button className={`button ${activeCategory === 'css' ? 'active' : ''}`} onClick={() => handleButtonClick('css')}>
+                    CSS
+                </button>
+            </Link>
+            <Link to="/javascript">
+                <button className={`button ${activeCategory === 'javascript' ? 'active' : ''}`} onClick={() => handleButtonClick('javascript')}>
+                    JavaScript
+                </button>
+            </Link>
+            <Link to="/react">
+                <button className={`button ${activeCategory === 'react' ? 'active' : ''}`} onClick={() => handleButtonClick('react')}>
+                    React
+                </button>
+            </Link>
+            <Link to="/headless-cms">
+                <button className={`button ${activeCategory === 'headless-cms' ? 'active' : ''}`} onClick={() => handleButtonClick('headless-cms')}>
+                    Sanity and headless CMS
+                </button>
+            </Link>
         </nav>
     )
 }
